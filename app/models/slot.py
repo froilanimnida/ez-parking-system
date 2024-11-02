291266a4ec07ed21a78daeb2c697b107fc77d0ec
uaenaaaa <61849282+uaenaaaa@users.noreply.github.com>
2024-11-02 21:55:16 +0800

    added model layer operation

--- app/models/slot.py
+++ app/models/slot.py
@@ -4,10 +4,12 @@ from sqlalchemy import (
     Column, Integer, VARCHAR, Enum, DateTime, ForeignKey, Boolean,
 )
 from sqlalchemy.orm import relationship
+from sqlalchemy.exc import OperationalError
 
 from app.models.base import Base
+from app.utils.engine import get_session
 
-class SlotModel(Base):
+class Slot(Base):
     """ Class model represents the model for the slot in the database. """
     __tablename__ = 'slot'
 
@@ -41,3 +43,51 @@ class SlotModel(Base):
         back_populates='slots',
         cascade='all, delete-orphan'
     )
+
+
+class GettingSlotsOperations:  # pylint: disable=R0903
+    """ Wraps the logic for getting the list of slots. """
+    @staticmethod
+    def get_all_slots():
+        """ Get all slots. This is for administrative purposes. """
+        session = get_session()
+        try:
+            slots = session.query(Slot).all()
+            return slots
+        except OperationalError as error:
+            raise error
+
+    @staticmethod
+    def get_slot_by_vehicle_type(vehicle_type_id: int, establishment_id: int):  # pylint: disable=C0116
+        session = get_session()
+        try:
+            slots = session.query(Slot).filter(
+                Slot.vehicle_type_id == vehicle_type_id,
+                Slot.establishment_id == establishment_id
+            ).all()
+            return slots
+        except OperationalError as error:
+            raise error
+
+
+    @staticmethod
+    def get_slot_by_establishment(establishment_id: int):  # pylint: disable=C0116
+        session = get_session()
+        try:
+            slots = session.query(Slot).filter(
+                Slot.establishment_id == establishment_id
+            ).all()
+            return slots
+        except OperationalError as error:
+            raise error
+
+    @staticmethod
+    def get_slot_by_slot_code(slot_code: str): # pylint: disable=C0116
+        session = get_session()
+        try:
+            slot = session.query(Slot).filter(
+                Slot.slot_code == slot_code
+            ).first()
+            return slot
+        except OperationalError as error:
+            raise error
