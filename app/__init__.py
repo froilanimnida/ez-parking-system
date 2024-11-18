75d7e491be1cc138742b34087853aeefd3d42db5
uaenaaaa <61849282+uaenaaaa@users.noreply.github.com>
2024-11-18 13:26:45 +0800

    fix: order of config initialization

--- app/__init__.py
+++ app/__init__.py
@@ -18,18 +18,16 @@ from app.utils.logger import setup_logging
 def create_app():
     """Factory function to create the Flask app instance."""
     template_dir = path.join(path.abspath(path.dirname(__file__)), "templates")
+
     app = Flask(__name__, template_folder=template_dir)
-
-    api.init_app(app)
-
     app.config.from_object(DevelopmentConfig)
 
-    mail.init_app(app)
+    api.init_app(app)
     JWTManager(app)
+    mail.init_app(app)
 
     setup_logging(app)
     register_system_wide_error_handlers(app)
     register_blueprints(api)
     add_jwt_after_request_handler(app)
-
     return app
