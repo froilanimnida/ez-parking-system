1140fe6598d9427239ed6daf25f19133f8e9975e
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2025-02-03 21:04:39 +0800

    feat: update CORS configuration to include dynamic frontend URLs and static origins

--- app/utils/setup_cors.py
+++ app/utils/setup_cors.py
@@ -8,19 +8,21 @@ from flask_cors import CORS
 
 def set_up_cors(app: Flask):
     """Set up CORS for the application."""
-    allowed_origins = [
-        getenv("FRONTEND_URL").split(","),
+    frontend_urls = getenv("FRONTEND_URL", "").split(",")
+    static_origins = [
         "https://ez-parking.expo.app",
         "https://ez-parking-gamma.vercel.app",
         "https://ez-parking.vercel.app"
     ]
+
+    allowed_origins = frontend_urls + static_origins
 
     CORS(
         app,
         supports_credentials=True,
         origins=allowed_origins,
         allow_headers=["Content-Type", "X-CSRF-TOKEN", "Accept", "Authorization"],
-        expose_headers=["Set-Cookie", "Authorization", ],
+        expose_headers=["Set-Cookie", "Authorization"],
         methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     )
     return app
